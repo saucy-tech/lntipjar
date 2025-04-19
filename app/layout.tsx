3314fb9e0aa6d1119387f1ec2af38f77698aa0b2
Stephen DeLorme <stephen@d.elor.me>
2025-04-19 08:11:10 -0400

    site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,23 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Lightning Tip Jar",
-  description: "Leave a lightning tip using LNBits",
+  title: "Lightning Tip Jar | ATL BitLab",
+  description: "Leave us a tip in bitcoin -- or deploy this code to run your own lightning tip jar!",
+  icons: {
+    icon: "/atl-bitlab-favicon.png",
+    apple: "/atl-bitlab-favicon.png",
+  },
+  openGraph: {
+    title: "Lightning Tip Jar | ATL BitLab",
+    description: "Leave us a tip in bitcoin -- or deploy this code to run your own lightning tip jar!",
+    images: ['/lightning-tip-jar.jpg'],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Lightning Tip Jar | ATL BitLab",
+    description: "Leave us a tip in bitcoin -- or deploy this code to run your own lightning tip jar!",
+    images: ['/lightning-tip-jar.jpg'],
+  },
 };
 
 export default function RootLayout({
