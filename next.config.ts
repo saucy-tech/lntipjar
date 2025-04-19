25f766a164db50557eacdfee7bd41356b40fafbd
Stephen DeLorme <stephen@d.elor.me>
2025-04-19 07:57:54 -0400

    remove unneeded env config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  env: {
-    APP_MODE: process.env.USE_REAL_LNBITS === 'true' ? 'real' : 'mock',
-  },
 };
 
 export default nextConfig;
